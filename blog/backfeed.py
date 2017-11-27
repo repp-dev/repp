9194d9274747a159afffbb57f614dc2fc1ad283c
root <root@ip-172-31-8-207.us-west-1.compute.internal>
2017-11-27 02:47:18 +0000

    rating traversal script working

--- blog/backfeed.py
+++ blog/backfeed.py
@@ -10,13 +10,30 @@ password = "neo4jneo4j"
 
 graph = Graph(url + '/db/data/', username=username, password=password)
 
-query = 'MATCH (user)-[rating:RATED_AUTHENTICITY]->(post) RETURN rating, user, post'
-
-actions = graph.run(query)
-    
-for action in actions:
+for action in graph.run('MATCH (user)-[rating:RATED_AUTHENTICITY]->(post) RETURN rating, user, post'):
     print(action['user']['username'] )
     print (action['rating']['rating'])
     print (action['post']['id'])
     print (action['post']['author'])
-    print ("------")
+    print ("------ AUTHENTICITY")
+    
+for action in graph.run('MATCH (user)-[rating:RATED_TRUST]->(post) RETURN rating, user, post'):
+    print(action['user']['username'] )
+    print (action['rating']['rating'])
+    print (action['post']['id'])
+    print (action['post']['author'])
+    print ("------ TRUST")
+    
+for action in graph.run('MATCH (user)-[rating:RATED_VALID]->(post) RETURN rating, user, post'):
+    print(action['user']['username'] )
+    print (action['rating']['rating'])
+    print (action['post']['id'])
+    print (action['post']['author'])
+    print ("------ VALID")
+    
+for action in graph.run('MATCH (user)-[rating:RATED_LIKE]->(post) RETURN rating, user, post'):
+    print(action['user']['username'] )
+    print (action['rating']['rating'])
+    print (action['post']['id'])
+    print (action['post']['author'])
+    print ("------ LIKE")
